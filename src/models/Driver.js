809605add3fb63c4ac51009262cdfabf81c58eb5
Calvin Ulis Howard Howard <Calvin.uhh@gmail.com>
2024-04-26 11:49:21 -0500

    Agregando server de express

--- src/models/Driver.js
+++ src/models/Driver.js
@@ -2,7 +2,7 @@ const { DataTypes, UUIDV4 } = require('sequelize');
 
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('', {
+  sequelize.define('Driver', {
     id: {
       type: DataTypes.UUID,
       defaultValue:UUIDV4,
--- src/models/Freigth.js
+++ src/models/Freigth.js
@@ -2,7 +2,7 @@ const { DataTypes, UUIDV4 } = require('sequelize');
 
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('', {
+  sequelize.define('Freigth', {
     id: {
       type: DataTypes.INTEGER,
       autoIncrement: true,
